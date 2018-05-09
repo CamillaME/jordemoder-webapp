8c2a4a66fcea55c5bb6cab7a6705c205dc196819
CamillaME <info@camillame.dk>
2018-05-09 17:54:53 +0200

    Refleksionsark

--- src/app/reflection/fill-out-reflection/fill-out-reflection.component.ts
+++ src/app/reflection/fill-out-reflection/fill-out-reflection.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fill-out-reflection',
   templateUrl: './fill-out-reflection.component.html',
-  styleUrls: ['./fill-out-reflection.component.css']
+  styleUrls: ['./fill-out-reflection.component.less']
 })
 export class FillOutReflectionComponent implements OnInit {
 
