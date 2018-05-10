a24bc146f50bde9543882a8582aefdf82daff11e
denra <denra82@gmail.com>
2018-05-10 15:53:14 +0200

    changes

--- src/app/schema/fill-out-schema/fill-out-schema.component.ts
+++ src/app/schema/fill-out-schema/fill-out-schema.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireDatabase, snapshotChanges } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
+export interface Experienceschema { name: string; dates: Date; }
+export interface ExperienceschemaId extends Experienceschema { id: string;}
 
 @Component({
   selector: 'app-fill-out-schema',
@@ -10,10 +15,21 @@ export class FillOutSchemaComponent implements OnInit {
 
   constructor(private db: AngularFirestore) {
     db.firestore.settings({ timestampsInSnapshots: true});
+    this.experienceSchemaCol = db.collection<Experienceschema>('expSchema');
+    this.experienceSchemas = this.experienceSchemaCol.snapshotChanges().map(actions => {
+      return actions.map(a=> {
+        const data = a.payload.doc.data() as Experienceschema;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      })
+    })
    }
 
-   OnAddDate(){
-   }
+  experienceSchemaCol: AngularFirestoreCollection<Experienceschema>;
+  experienceSchemas: Observable<ExperienceschemaId[]>;
+
+  OnAddDate(){
+  }
 
   ngOnInit() {
   }
