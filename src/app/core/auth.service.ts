f7fa2830c070f51ff28318a0e7d2d3c648c77fba
denra <denra82@gmail.com>
2018-05-13 13:49:01 +0200

    service

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root'
-})
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap'
+
+interface User {
+  uid: string;
+  email: string;
+  displayName: string;
+}
+
+@Injectable()
 export class AuthService {
 
   constructor() { }
