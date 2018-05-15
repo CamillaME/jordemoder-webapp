8c7bf9d14ab3c65f2d811e643daac608a34ed47b
CamillaME <info@camillame.dk>
2018-05-15 17:54:34 +0200

    Download pdf

--- src/app/reflection/previous-reflection/previous-reflection.component.ts
+++ src/app/reflection/previous-reflection/previous-reflection.component.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs/Observable';
 import { ReflectionService } from '../../Shared/reflection.service';
 import { Router } from "@angular/router";
 
+import * as jsPDF from 'jspdf';
+
 @Component({
   selector: 'app-previous-reflection',
   templateUrl: './previous-reflection.component.html',
@@ -32,5 +34,33 @@ export class PreviousReflectionComponent implements OnInit {
 
   reflectionChangedHandler(reflections) {
     this.reflections = reflections;
+  }
+
+  onDownload(title, id) {
+    const elementToPrint = document.getElementById('reflection-' + id); //The html element to become a pdf
+
+    // var doc = new jsPDF();
+    // const pdf = new jsPDF();
+    // pdf.addHTML(elementToPrint, () => {
+    //   pdf.save(title);
+    // });
+
+    var doc = new jsPDF();
+
+    // We'll make our own renderer to skip this editor
+    var specialElementHandlers = {
+      '#editor': function (element, renderer) {
+        return true;
+      }
+    };
+
+    // All units are in the set measurement for the document
+    // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
+    doc.fromHTML((elementToPrint), 15, 15, {
+      'width': 170,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save(title);
   }
 }
