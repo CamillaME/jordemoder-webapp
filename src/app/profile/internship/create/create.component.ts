3a2c260cdf00dcfbf3bd2bdeb98d779574122c6a
denra <denra82@gmail.com>
2018-05-21 11:41:50 +0200

    changes

--- src/app/profile/internship/create/create.component.ts
+++ src/app/profile/internship/create/create.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AngularFireDatabase, snapshotChanges } from 'angularfire2/database';
+import { FirebaseApp } from 'angularfire2';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { firestore } from 'firebase';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +14,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateInternshipComponent implements OnInit {
 
-  constructor() { }
+  term: string;
+  place: string;
+  street: string;
+  zip: number;
+  city: string;
+  midwifeName: string;
+  midwifePhoneNumber: string;
+  midwifeEmail: string;
+  internshipSupervisorName: string;
+  internshipSupervisorPhone: string;
+  internshipSupervisorEmail: string;
+  internshipTeacherName: string;
+  internshipTeacherPhone: string;
+  internshipTeacherEmail: string;
+
+  internshipsCol: AngularFirestoreCollection<any>;
+  internships: Observable<any[]>;
+
+  constructor(private db: AngularFirestore) {
+    db.firestore.settings({ timestampsInSnapshots: true });
+  }
+
+  onsubmitIntership() {
+  }
 
   ngOnInit() {
+    this.internshipsCol = this.db.collection('Internships');
+    this.internships = this.internshipsCol.valueChanges();
   }
 
 }
