b8b3094ae684ce86bc27a43f9b3e6ed447d5c644
denra <denra82@gmail.com>
2018-05-27 13:22:13 +0200

    changes

--- src/app/schema/fill-out-schema/fill-out-schema.component.ts
+++ src/app/schema/fill-out-schema/fill-out-schema.component.ts
@@ -11,9 +11,6 @@ import { Route, ActivatedRoute } from '@angular/router';
 import * as firebase from 'firebase';
 import { first } from 'rxjs/operator/first';
 
-// export interface Experienceschema { name: string; dates: Date; }
-// export interface ExperienceschemaId extends Experienceschema { id: string; }
-
 @Component({
   selector: 'app-fill-out-schema',
   templateUrl: './fill-out-schema.component.html',
@@ -27,15 +24,66 @@ export class FillOutSchemaComponent implements OnInit {
   date2: Date;
   date3: Date;
   date4: Date;
+  date5: Date;
+  date6: Date;
+  date7: Date;
+  date8: Date;
+  date9: Date;
+  date10: Date;
+  date11: Date;
+  date12: Date;
+  date13: Date;
+  date14: Date;
+  date15: Date;
+  date16: Date;
+  date17: Date;
+  date18: Date;
+  date19: Date;
+  date20: Date;
+  date21: Date;
+  date22: Date;
+  date23: Date;
+  date24: Date;
+  date25: Date;
+  date26: Date;
+  date27: Date;
+  date28: Date;
+  date29: Date;
+  date30: Date;
+  date31: Date;
+  date32: Date;
   moreDates: string = "";
   moreDates2: string = "";
   moreDates3: string = "";
   moreDates4: string = "";
-  textDatetest: string;
-  newDate: string;
-  newDate2: string;
-  newDate3: string;
-  newDate4: string;
+  moreDates5: string = "";
+  moreDates6: string = "";
+  moreDates7: string = "";
+  moreDates8: string = "";
+  moreDates9: string = "";
+  moreDates10: string = "";
+  moreDates11: string = "";
+  moreDates12: string = "";
+  moreDates13: string = "";
+  moreDates14: string = "";
+  moreDates15: string = "";
+  moreDates16: string = "";
+  moreDates17: string = "";
+  moreDates18: string = "";
+  moreDates19: string = "";
+  moreDates20: string = "";
+  moreDates21: string = "";
+  moreDates22: string = "";
+  moreDates23: string = "";
+  moreDates24: string = "";
+  moreDates25: string = "";
+  moreDates26: string = "";
+  moreDates27: string = "";
+  moreDates28: string = "";
+  moreDates29: string = "";
+  moreDates30: string = "";
+  moreDates31: string = "";
+  moreDates32: string = "";
   docData: string;
   profiles;
   studentNumber: number;
@@ -78,230 +126,202 @@ export class FillOutSchemaComponent implements OnInit {
         'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates4
     });
   }
-  // OnAddDate5(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate5 = this.date5.toString();
-  //   this.moreDates5 = Dates + " " + this.newDate5;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'ObservationAfBristningOgEpisisotomi': this.moreDates5
-  //   });
-  // }
-  // OnAddDate6(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate6 = this.date6.toString();
-  //   this.moreDates6 = Dates + " " + this.newDate6;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningVedBristningOgEpisisotomi': this.moreDates6
-  //   });
-  // }
-  // OnAddDate7(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate7 = this.date7.toString();
-  //   this.moreDates7 = Dates + " " + this.newDate7;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'PalpationAfUterus': this.moreDates7
-  //   });
-  // }
-  // OnAddDate8(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate8 = this.date8.toString();
-  //   this.moreDates8 = Dates + " " + this.newDate8;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'ObservationOgVejledningOmkringLokkier': this.moreDates8
-  //   });
-  // }
-  // OnAddDate9(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate9 = this.date9.toString();
-  //   this.moreDates9 = Dates + " " + this.newDate9;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'Observartionafvandladning': this.moreDates9
-  //   });
-  // }
-  // OnAddDate10(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate10 = this.date10.toString();
-  //   this.moreDates10 = Dates + " " + this.newDate10;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates10
-  //   });
-  // }
-  // OnAddDate11(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate11 = this.date11.toString();
-  //   this.moreDates11 = Dates + " " + this.newDate11;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates11
-  //   });
-  // }
-  // OnAddDate12(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate12 = this.date12.toString();
-  //   this.moreDates12 = Dates + " " + this.newDate12;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates12
-  //   });
-  // }
-  // OnAddDate13(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate13 = this.date13.toString();
-  //   this.moreDates13 = Dates + " " + this.newDate13;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates13
-  //   });
-  // }
-  // OnAddDate14(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate14 = this.date14.toString();
-  //   this.moreDates14 = Dates + " " + this.newDate14;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates14
-  //   });
-  // }
-  // OnAddDate15(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate15 = this.date15.toString();
-  //   this.moreDates15 = Dates + " " + this.newDate15;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates15
-  //   });
-  // }
-  // OnAddDate16(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate16 = this.date16.toString();
-  //   this.moreDates16 = Dates + " " + this.newDate16;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates16
-  //   });
-  // }
-  // OnAddDate17(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate17 = this.date17.toString();
-  //   this.moreDates17 = Dates + " " + this.newDate17;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates17
-  //   });
-  // }
-  // OnAddDate18(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate18 = this.date18.toString();
-  //   this.moreDates18 = Dates + " " + this.newDate18;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates18
-  //   });
-  // }
-  // OnAddDate19(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate19 = this.date19.toString();
-  //   this.moreDates19 = Dates + " " + this.newDate19;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates19
-  //   });
-  // }
-  // OnAddDate20(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate20 = this.date20.toString();
-  //   this.moreDates20 = Dates + " " + this.newDate20;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates20
-  //   });
-  // }
-  // OnAddDate21(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate21 = this.date21.toString();
-  //   this.moreDates4 = Dates + " " + this.newDate21;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates21
-  //   });
-  // }
-  // OnAddDate22(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate22 = this.date22.toString();
-  //   this.moreDates22 = Dates + " " + this.newDate22;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates22
-  //   });
-  // }
-  // OnAddDate23(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate23 = this.date23.toString();
-  //   this.moreDate23 = Dates + " " + this.newDate23;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates23
-  //   });
-  // }
-  // OnAddDate24(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate24 = this.date24.toString();
-  //   this.moreDates24 = Dates + " " + this.newDate24;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates24
-  //   });
-  // }
-  // OnAddDate25(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate25 = this.date25.toString();
-  //   this.moreDates25 = Dates + " " + this.newDate25;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates25
-  //   });
-  // }
-  // OnAddDate25(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate25 = this.date25.toString();
-  //   this.moreDates25 = Dates + " " + this.newDate25;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates25
-  //   });
-  // }
-  // OnAddDate26(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate26 = this.date26.toString();
-  //   this.moreDates26 = Dates + " " + this.newDate26;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates26
-  //   });
-  // }
-  // OnAddDate27(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate27 = this.date27.toString();
-  //   this.moreDates27 = Dates + " " + this.newDate27;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates27
-  //   });
-  // }
-  // OnAddDate28(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate28 = this.date28.toString();
-  //   this.moreDates28 = Dates + " " + this.newDate28;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates28
-  //   });
-  // }
-  // OnAddDate29(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate29 = this.date29.toString();
-  //   this.moreDates29 = Dates + " " + this.newDate29;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates29
-  //   });
-  // }
-  // OnAddDate30(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate30 = this.date30.toString();
-  //   this.moreDates30 = Dates + " " + this.newDate30;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates30
-  //   });
-  // }
-  // OnAddDate31(id, Dates) {
-  //   this.docID = id;
-  //   this.newDate31 = this.date31.toString();
-  //   this.moreDates31 = Dates + " " + this.newDate31;
-  //   this.db.collection('Experienceschema').doc(this.docID).update({
-  //       'VejledningOmKvindesFysiologiskeForandringerpp': this.moreDates31
-  //   });
-  // }
+  OnAddDate5(id, Dates) {
+    this.docID = id;
+    this.moreDates5 = Dates + " " + this.date5.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'ObservationAfBristningOgEpisisotomi': this.moreDates5
+    });
+  }
+  OnAddDate6(id, Dates) {
+    this.docID = id;
+    this.moreDates6 = Dates + " " + this.date6.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledningVedBristningOgEpisisotomi': this.moreDates6
+    });
+  }
+  OnAddDate7(id, Dates) {
+    this.docID = id;
+    this.moreDates7 = Dates + " " + this.date7.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'PalpationAfUterus': this.moreDates7
+    });
+  }
+  OnAddDate8(id, Dates) {
+    this.docID = id;
+    this.moreDates8 = Dates + " " + this.date8.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'ObservationOgVejledningOmkringLokkier': this.moreDates8
+    });
+  }
+  OnAddDate9(id, Dates) {
+    this.docID = id;
+    this.moreDates9 = Dates + " " + this.date9.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'Observartionafvandladning': this.moreDates9
+    });
+  }
+  OnAddDate10(id, Dates) {
+    this.docID = id;
+    this.moreDates10 = Dates + " " + this.date10.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'GiveOrienteringOgVejledningIKnibeoevelser': this.moreDates10
+    });
+  }
+  OnAddDate11(id, Dates) {
+    this.docID = id;
+    this.moreDates11 = Dates + " " + this.date11.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledningIAmmeteknik': this.moreDates11
+    });
+  }
+  OnAddDate12(id, Dates) {
+    this.docID = id;
+    this.moreDates12 = Dates + " " + this.date12.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'ObservationAfOgVejledeVedBrystspaending': this.moreDates12
+    });
+  }
+  OnAddDate13(id, Dates) {
+    this.docID = id;
+    this.moreDates13 = Dates + " " + this.date13.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'ForebyggelseOgPlejeAfOemmePapiller': this.moreDates13
+    });
+  }
+  OnAddDate14(id, Dates) {
+    this.docID = id;
+    this.moreDates14 = Dates + " " + this.date14.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'ForebyggelseOgPlejeAfPapillerMedFissurer': this.moreDates14
+    });
+  }
+  OnAddDate15(id, Dates) {
+    this.docID = id;
+    this.moreDates15 = Dates + " " + this.date15.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledningObservationOmkringMastitis': this.moreDates15
+    });
+  }
+  OnAddDate16(id, Dates) {
+    this.docID = id;
+    this.moreDates16 = Dates + " " + this.date16.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledningOmSuttebriks': this.moreDates16
+    });
+  }
+  OnAddDate17(id, Dates) {
+    this.docID = id;
+    this.moreDates17 = Dates + " " + this.date17.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledningOmGiveMaelkPaaKop': this.moreDates17
+    });
+  }
+  OnAddDate18(id, Dates) {
+    this.docID = id;
+    this.moreDates18 = Dates + " " + this.date18.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledningIUdmalkningManueltBrystpumpeHaandpumpe': this.moreDates18
+    });
+  }
+  OnAddDate19(id, Dates) {
+    this.docID = id;
+    this.moreDates19 = Dates + " " + this.date19.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'AmningAfGemelli': this.moreDates19
+    });
+  }
+  OnAddDate20(id, Dates) {
+    this.docID = id;
+    this.moreDates20 = Dates + " " + this.date20.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledeMorDerIkkeOenskerAtAmme': this.moreDates20
+    });
+  }
+  OnAddDate21(id, Dates) {
+    this.docID = id;
+    this.moreDates4 = Dates + " " + this.date21.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'BrystopereredeKvinderOgAmning': this.moreDates21
+    });
+  }
+  OnAddDate22(id, Dates) {
+    this.docID = id;
+    this.moreDates22 = Dates + " " + this.date22.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'HypoHypergalakti': this.moreDates22
+    });
+  }
+  OnAddDate23(id, Dates) {
+    this.docID = id;
+    this.moreDates23 = Dates + " " + this.date23.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledeIVitaminTilskudTilBarnet': this.moreDates23
+    });
+  }
+  OnAddDate24(id, Dates) {
+    this.docID = id;
+    this.moreDates24 = Dates + " " + this.date24.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledeIDenNormaleBarselspsyke': this.moreDates24
+    });
+  }
+  OnAddDate25(id, Dates) {
+    this.docID = id;
+    this.moreDates25 = Dates + " " + this.date25.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledeISoeskendeJalousi': this.moreDates25
+    });
+  }
+  OnAddDate32(id, Dates) {
+    this.docID = id;
+    this.moreDates32 = Dates + " " + this.date32.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'SamtaleOmFamiliensNyeStruktur': this.moreDates32
+    });
+  }
+  OnAddDate26(id, Dates) {
+    this.docID = id;
+    this.moreDates26 = Dates + " " + this.date26.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'SamtaleOmSexOgSamlivEfterFoedslenHerunderPraevention': this.moreDates26
+    });
+  }
+  OnAddDate27(id, Dates) {
+    this.docID = id;
+    this.moreDates27 = Dates + " " + this.date27.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledningOmBarnetsBehovSignaler': this.moreDates27
+    });
+  }
+  OnAddDate28(id, Dates) {
+    this.docID = id;
+    this.moreDates28 = Dates + " " + this.date28.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledningVedrForaeldreBarnRelation': this.moreDates28
+    });
+  }
+  OnAddDate29(id, Dates) {
+    this.docID = id;
+    this.moreDates29 = Dates + " " + this.date29.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'ObservationAfOgOmsorgForDetNyfoedteBarn': this.moreDates29
+    });
+  }
+  OnAddDate30(id, Dates) {
+    this.docID = id;
+    this.moreDates30 = Dates + " " + this.date30.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'VejledningOmkringSpaedbarnspleje': this.moreDates30
+    });
+  }
+  OnAddDate31(id, Dates) {
+    this.docID = id;
+    this.moreDates31 = Dates + " " + this.date31.toString();
+    this.db.collection('Experienceschema').doc(this.docID).update({
+        'AffoeringOgVandladning': this.moreDates31
+    });
+  }
 
   ngOnInit() {
     let self = this;
